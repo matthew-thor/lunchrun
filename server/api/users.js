56c00ce004b6a8218859456fac900b6d53d9fc8c
Matthew Thor <mjthor@gmail.com>
2017-12-20 14:09:58 -0600

    test existing /api/users routes

--- server/api/users.js
+++ server/api/users.js
@@ -7,16 +7,15 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email', 'firstName', 'lastName', 'admin']
+    attributes: ['id', 'email', 'firstName', 'lastName', 'admin'],
   })
     .then(users => res.json(users))
     .catch(next);
 });
 
 router.get('/:id', (req, res, next) => {
-  console.log('session', req.session.id);
-  User.find({ where: {id: req.params.id},
-    attributes: ['id', 'email', 'firstName', 'lastName', 'admin']})
+  User.findById(req.params.id,
+    { attributes: ['id', 'email', 'firstName', 'lastName', 'admin'] })
     .then(user => res.json(user))
     .catch(next);
 });
@@ -24,14 +23,17 @@ router.get('/:id', (req, res, next) => {
 router.put('/:id', async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
-    user.update(req.body);
-    res.sendStatus(202);
+    await user.update(req.body);
+    res.sendStatus(204);
   }
   catch (err) { next(err); }
 });
 
 router.post('/', async (req, res, next) => {
-  try { res.json(await User.create(req.body)); }
+  try {
+    const createdUser = await User.create(req.body);
+    res.status(201).json(createdUser);
+  }
   catch (err) { next(); }
 });
 
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const db = require('./db');
-const sessionStore = new SequelizeStore({db});
+const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 const app = express();
 const socketio = require('socket.io');
@@ -32,7 +32,9 @@ passport.deserializeUser((id, done) =>
 
 const createApp = () => {
   // logging middleware
-  app.use(morgan('dev'));
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(morgan('dev'));
+  }
 
   // body parsing middleware
   app.use(bodyParser.json());
@@ -46,7 +48,7 @@ const createApp = () => {
     secret: process.env.SESSION_SECRET || 'my best friend is Cody',
     store: sessionStore,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   }));
   app.use(passport.initialize());
   app.use(passport.session());
@@ -61,13 +63,13 @@ const createApp = () => {
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
     } else {
-      next()
+      next();
     }
-  })
+  });
 
   // sends index.html
   app.use('*', (req, res) => {
