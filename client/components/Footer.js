b594002ebaf3a603d2e2abf27073b93854859b63
Matthew Thor <mjthor@gmail.com>
2018-01-26 10:42:05 -0600

    initial file

---
+++ client/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Footer = () => {
+  return (
+    <div className="inner cover landing">
+      <h1 className="cover-heading">Loop Lunch Run</h1>
+      <p className="lead">Making Lou's life easier, one website at a time.</p>
+      <p className="lead">
+        <Link to="/login" className="btn btn-lg btn-default">Log in</Link>
+        <span className="spacer" />
+        <Link to="/signup" className="btn btn-lg btn-default">Sign up</Link>
+      </p>
+    </div>
+  );
+};
+
+// const mapState = state => ({ user: state.user });
+
+// export default connect(mapState)(Landing);
+
+export default Footer;
+/**
+ * PROP TYPES
+ */
+Landing.propTypes = { user: PropTypes.object };
